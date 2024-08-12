996b6fab2abd9d5e0099e319930731d1a77ea934
Enriquezgithub <140429809+Enriquezgithub@users.noreply.github.com>
2024-08-12 19:06:33 +0800

    make the dashboard responsive

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -9,14 +9,14 @@
         Good morning {{ Auth::user()->name }}
     </div>
 
-    <div class="flex gap-10">
-        <div class="bg-white shadow rounded w-[25%] mt-3 p-2 h-[20vh]">
+    <div class="flex gap-4">
+        <div class="bg-white shadow rounded w-[50%] md:w-[25%] mt-3 p-2 h-[20vh]">
             <div class="text-lg font-bold">
                 Total Patient
             </div>
         </div>
 
-        <div class="bg-white shadow rounded w-[25%] mt-3 p-2 h-[20vh]">
+        <div class="bg-white shadow rounded w-[50%] md:w-[25%] mt-3 p-2 h-[20vh]">
             <div class="text-lg font-bold">
                 New Patient
             </div>
